35bf8a9f51fd8fe2a56dc0244f35112592dd456d
Efren Rodriguez <efren.rodriguez.sa@gmail.com>
2022-03-19 11:25:44 -0600

    primer commit

---
+++ src/test/java/com/fundamentosPlatzi/Springboot/fundamentos/FundamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundamentosPlatzi.Springboot.fundamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
